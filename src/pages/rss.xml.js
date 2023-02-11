bf007469e2373661b336925ece81f3422f419a00
dorakika <1633198089@qq.com>
2023-02-11 22:22:10 +0800

    fix: rss 字段

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,6 +10,7 @@ export async function get(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
+			pubDate: post.data.date,
 			link: `/posts/${post.slug}/`,
 		})),
 	});
