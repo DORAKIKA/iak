8f80b65bfa5ff30b7e078116c35d3094228235ea
dorakika <1633198089@qq.com>
2023-02-23 12:32:30 +0800

    feat(robot): robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,26 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
-import { site } from "./src/config"
+import { site } from "./src/config";
 
 // yaml
-import yaml from "@rollup/plugin-yaml"
+import yaml from "@rollup/plugin-yaml";
 
-
+// https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: site.origin,
-  integrations: [mdx(), sitemap(), vue()],
-
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    vue(), 
+    robotsTxt({
+      // host 用于配置多镜像网站的首选域 - 设为true则使用上方的site配置项
+      host: true,
+    })
+  ],
   vite: {
     plugins: [yaml()]
   }
