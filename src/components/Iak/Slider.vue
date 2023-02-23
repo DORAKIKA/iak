cbab59ae45c44f4befb16c4ff02392d526746200
dorakika <1633198089@qq.com>
2023-02-23 11:20:05 +0800

    feat(components): 一个离散滑块

---
+++ src/components/Iak/Slider.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+interface SliderProps{
+    min: number,
+    max: number,
+    step: number,
+    value: number,
+}
+const props = defineProps<SliderProps>();
+const emits = defineEmits<{
+    (e: 'update:value', value: number): void
+}>()
+
+const values:number[] = [];
+for(let i = props.min; i <= props.max; i+=props.step){
+    values.push(Math.round(i*10)/10);
+}
+
+</script>
+
+<template>
+    <div class="iak-slider">
+        <div class="button"
+        v-for="value in values"
+        :key="value"
+        :title="value.toString()"
+        @click="() => emits('update:value', value)"
+        :class="value === props.value ? 'active' : ''"
+        ></div>
+    </div>
+</template>
+
+<style scoped>
+.iak-slider{
+    height: 4px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 8px;
+    background: var(--light-bg);
+}
+.iak-slider .button{
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background: var(--light-bg);
+    cursor: pointer;
+    transition: 0.3s;
+}
+.iak-slider .button.active{
+    width: 16px;
+    height: 16px;
+    background: var(--deep-bg);
+}
+</style>
