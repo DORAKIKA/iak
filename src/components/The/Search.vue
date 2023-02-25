85cf521205b84989e5692bb86656adf368c94161
dorakika <1633198089@qq.com>
2023-02-25 20:46:28 +0800

    refactor: header部分vue组件转为astro，优化首屏加载

--- src/components/The/Search.vue
+++ src/components/The/Search.vue
@@ -36,15 +36,11 @@ const toggleSearch = (value:boolean|undefined) => {
 
 query({target:{value:''}})
 
-defineExpose({
-    toggleSearch
-})
+// @ts-ignore
+window.iak.toggleSearch = toggleSearch
 </script>
 
 <template>
-    <svg t="1670941746641" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5314" width="128" height="128">
-        <path d="M1005.312 914.752l-198.528-198.464A448 448 0 1 0 0 448a448 448 0 0 0 716.288 358.784l198.4 198.4a64 64 0 1 0 90.624-90.432zM448 767.936A320 320 0 1 1 448 128a320 320 0 0 1 0 640z" fill="#262626" p-id="5315"></path>
-    </svg>
     <teleport to='body'>
         <transition name="pop">
             <div class="the-search-wrapper" v-show="searchShow" @click.self="toggleSearch(false)">
--- src/components/The/Sidebar.vue
+++ src/components/The/Sidebar.vue
@@ -1,34 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import IakDrawer from "@components/Iak/drawer.vue"
-interface Menu {
-    name: string
-    children: MenuItem[]
-}
-interface MenuItem {
-    name: string
-    href: string
-}
-interface Props{
-    menus: Array<Menu>
-}
-const props = defineProps<Props>()
+import { menus } from 'src/config';
+
 
 const sidebarShow = ref(false)
 const toggleSidebar = () => {
     sidebarShow.value = !sidebarShow.value;
 }
-defineExpose({
-    toggleSidebar
-})
+// @ts-ignore
+window.iak.toggleSidebar = toggleSidebar
 </script>
 <template>
-    <svg t="1671348868662" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="10746" width="32" height="32"><path d="M950.848 768l0 73.152q0 14.848-10.848 25.728t-25.728 10.848l-804.576 0q-14.848 0-25.728-10.848t-10.848-25.728l0-73.152q0-14.848 10.848-25.728t25.728-10.848l804.576 0q14.848 0 25.728 10.848t10.848 25.728zM950.848 475.424l0 73.152q0 14.848-10.848 25.728t-25.728 10.848l-804.576 0q-14.848 0-25.728-10.848t-10.848-25.728l0-73.152q0-14.848 10.848-25.728t25.728-10.848l804.576 0q14.848 0 25.728 10.848t10.848 25.728zM950.848 182.848l0 73.152q0 14.848-10.848 25.728t-25.728 10.848l-804.576 0q-14.848 0-25.728-10.848t-10.848-25.728l0-73.152q0-14.848 10.848-25.728t25.728-10.848l804.576 0q14.848 0 25.728 10.848t10.848 25.728z" p-id="10747"></path></svg>
     <IakDrawer v-model:show="sidebarShow" class="the-sidebar">
         <template #title><span>菜单</span></template>
         <template #default>
             <div class="menus">
-                <div class="menu" v-for="menu in props.menus" :key="menu.name">
+                <div class="menu" v-for="menu in menus" :key="menu.name">
                     <div class="menu-name">{{menu.name}}</div>
                     <div class="menu-items">
                         <a class="menu-item" v-for="menuItem in menu.children" :href="menuItem.href">{{menuItem.name}}</a>
