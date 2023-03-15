55afe3d050da6e4c5f7c6032e38d95fe16d29546
dorakika <1633198089@qq.com>
2023-03-15 22:58:52 +0800

    feat: 文章页添加字数统计

--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -4,3 +4,10 @@ export const formatDateTime = (date: Date) => {
     const day = date.getDate();
     return `${year}-${month > 9 ? month : '0'+month}-${day > 9 ? day : '0'+day}`;
 }
+
+export const count = (str: string) => {
+    // 假装很正式，其实这些字符都是我即兴输的。
+    let rep = /[~|`|!|@|#|\$|%|\^|&|*|\(|\)|\-|=|_|\+|\[|\]|\{|\}|:|;|'|"|,|\.|<|>|/|?|\s|：|；|（|）|“|‘|？|·|！|￥|……|【|】]/g
+    let s = str.replace(rep, '');
+    return s.length;
+}
